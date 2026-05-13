a5f8a0e1a3d44cf14940cf7aeac3b79889268d29
Aakashkalla <aakashpradeepkalla@gmail.com>
2026-05-13 11:30:23 +0530

    Improved Spacing around Navbar and Button

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -3,7 +3,7 @@ import Button from "./ui/Button"
 
 const NavBar = () => {
     return (
-        <nav className="sticky top-0 z-50 bg-neutral-900/70 backdrop-blur-md text-neutral-600 w-full font-inter p-4 px-10">
+        <nav className="sticky top-0 z-50 bg-neutral-900/70 backdrop-blur-md text-neutral-600 w-full font-inter p-8 px-10">
             <div className="flex items-center justify-between">
                 {/* Heading */}
                 <h1 className="text-2xl text-primary font-montserrat font-semibold">PomoPivot</h1>
@@ -18,7 +18,7 @@ const NavBar = () => {
                 </div>
 
                 {/* CTA */}
-                <Button color="primary" href="" text="Start Focusing" classname="px-8 py-2 font-semibold"></Button>
+                <Button color="primary" href="" text="Start Focusing" classname="tracking-tight px-10 py-4 font-semibold"></Button>
             </div>
         </nav>
     )
